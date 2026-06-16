f0419795d90f55ba46d208a67bcfd90125a39841
Leo Farias <leoafarias@gmail.com>
2026-06-16 18:08:34 -0400

    feat: add jni to FFI plugin list and update dependencies in pubspec.lock

--- demo/macos/Flutter/GeneratedPluginRegistrant.swift
+++ demo/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,6 @@ import FlutterMacOS
 import Foundation
 
 import file_saver
-import path_provider_foundation
 import screen_retriever_macos
 import sqflite_darwin
 import webview_flutter_wkwebview
@@ -14,7 +13,6 @@ import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSaverPlugin.register(with: registry.registrar(forPlugin: "FileSaverPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
