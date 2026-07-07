582108a82630a3aa48dbb7c6a2f0a90e4092adb6
Leo Farias <leoafarias@gmail.com>
2026-07-07 19:04:48 -0400

    feat: re-add window_manager for desktop window sizing (#86)

--- packages/playground/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/playground/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,15 +6,19 @@ import FlutterMacOS
 import Foundation
 
 import audioplayers_darwin
+import screen_retriever_macos
 import sqflite_darwin
 import url_launcher_macos
 import video_player_avfoundation
 import webview_flutter_wkwebview
+import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   VideoPlayerPlugin.register(with: registry.registrar(forPlugin: "VideoPlayerPlugin"))
   WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
+  WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
