a6bdd188fb1fbde089e4b17791f614e5fb3d3076
Leo Farias <leoafarias@gmail.com>
2026-09-22 23:25:53 -0500

    refactor(superdeck): vendor the syntax highlighter; drop CocoaPods (#126)

--- packages/playground/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/playground/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,12 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import audioplayers_darwin
-import device_info_plus
 import file_saver
-import irondash_engine_context
 import screen_retriever_macos
 import sqflite_darwin
-import super_native_extensions
 import url_launcher_macos
 import video_player_avfoundation
 import webview_flutter_wkwebview
@@ -19,12 +16,9 @@ import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
-  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FileSaverPlugin.register(with: registry.registrar(forPlugin: "FileSaverPlugin"))
-  IrondashEngineContextPlugin.register(with: registry.registrar(forPlugin: "IrondashEngineContextPlugin"))
   ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
-  SuperNativeExtensionsPlugin.register(with: registry.registrar(forPlugin: "SuperNativeExtensionsPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   VideoPlayerPlugin.register(with: registry.registrar(forPlugin: "VideoPlayerPlugin"))
   WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
